63107d469f821dcf654dea7938d296384d1b9256
devameer <ameerafch@gmail.com>
2021-10-18 19:09:07 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-    $('.testimonials .owl-carousel').owlCarousel({
-        loop: true,
-        nav: true,
-        autoplay: true,
-        slideTransition: 'linear',
-        responsive: {
-            0: {
-                items: 1
-            }
-        }
-    })
-})
+  $(".testimonials .owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    autoplay: true,
+    slideTransition: "linear",
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+  });
+});
+$(".lazyload").lazy();
