3d00dd5f2da52fda016feef211d18b643d496208
FatmaZaher <fatoomzaher1999@gmail.com>
2021-10-18 17:27:25 +0300

    add

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.testimonials .owl-carousel').owlCarousel({
+        loop: true,
+        nav: true,
+        autoplay: true,
+        slideTransition: 'linear',
+        responsive: {
+            0: {
+                items: 1
+            }
+        }
+    })
+})
